4fdc263883004821b854d8f52b6f1287d3a107b6
CelticBoozer <celtic.boozer@proton.me>
2024-05-30 12:30:30 +0300

    init.lua formatted too.

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- lazy.nvim package manager installation
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,28 +16,28 @@ vim.opt.rtp:prepend(lazypath)
 require("core.options")
 
 local opts = {
-	defaults = {
-		lazy = false,
-		version = "*",
-		cond = nil,
-	},
-	dev = {
-		path = "~/Development/Projects",
-	},
-	install = {
-		missing = true,
-		colorscheme = { "gruvbox-materal" },
-	},
-	ui = {
-		border = "rounded",
-	},
+  defaults = {
+    lazy = false,
+    version = "*",
+    cond = nil,
+  },
+  dev = {
+    path = "~/Development/Projects",
+  },
+  install = {
+    missing = true,
+    colorscheme = { "gruvbox-materal" },
+  },
+  ui = {
+    border = "rounded",
+  },
 }
 
 local plugins = {
-	{ import = "plugin.editor" },
-	{ import = "plugin.lsp" },
-	{ import = "plugin.ui" },
-	{ import = "plugin.util" },
+  { import = "plugin.editor" },
+  { import = "plugin.lsp" },
+  { import = "plugin.ui" },
+  { import = "plugin.util" },
 }
 
 require("lazy").setup(plugins, opts)
